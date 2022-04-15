b45d8df4e3d6a8b137ef52066543f5b4befab306
Adham Nugroho <76804759+adhamnugroho@users.noreply.github.com>
2022-04-15 17:20:26 +0700

    Adding some diretory and some files

---
+++ src/php/output.php
@@ -0,0 +1,68 @@
+<!-- Apabila user belum mengisikan form registrasi, maka user akan dikembalikan ke halaman tambah.php -->
+<?php
+ if ( !isset($_POST["nama"]) ||
+     !isset($_POST["nis"]) ||
+     !isset($_POST["alamat"]) ||
+     !isset($_POST["jenisKelamin"]) ||
+     !isset($_POST["agama"])) {
+
+    //  redirect
+    header('Location: tambah.php');
+    exit;
+ } 
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/output.css">
+    <title></title>
+</head>
+<body>
+    <?php
+     $nama = $_POST['nama'];
+     $nis = $_POST['nis'];
+     $alamat = $_POST['alamat'];
+     $jenis_kelamin = $_POST['jenisKelamin'];
+     $agama = $_POST['agama'];
+    ?>
+
+    <div class="container">
+
+        <h1>Data Siswa</h1>
+
+        <table cellpadding="10">
+            <tr>
+                <td>Nama</td>
+                <td>:</td>
+                <td><?= $nama; ?></td>
+            </tr>
+            <tr>
+                <td>NIS</td>
+                <td>:</td>
+                <td><?= $nis; ?></td>
+            </tr>
+            <tr>
+                <td>Alamat</td>
+                <td>:</td>
+                <td><?= $alamat; ?></td>
+            </tr>
+            <tr>
+                <td>Jenis Kelamin</td>
+                <td>:</td>
+                <td><?= $jenis_kelamin; ?></td>
+            </tr>
+            <tr>
+                <td>Agama</td>
+                <td>:</td>
+                <td><?= $agama; ?></td>
+            </tr>
+        </table>
+    </div>
+
+</body>
+</html>
---
+++ src/php/tambah.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/tambah.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+
+        <h1>Registrasi Data Siswa</h1>
+
+        <form action="output.php" method="post">
+            <table cellpadding="11">
+                <tr class="nama">
+                    <td>
+                        <label for="nama">Nama</label>
+                    </td>
+                    <td>
+                        <label for="nama" class="label-titik-dua"> : </label>
+                    </td>
+                    <td>
+                        <input type="text" name="nama" id="nama" class="input-nama" required>
+                    </td>
+                </tr>
+                <tr class="nis">
+                    <td>
+                        <label for="nis">NIS</label>
+                    </td>
+                    <td>
+                        <label for="nis" class="label-titik-dua"> : </label>
+                    </td>
+                    <td>
+                        <input type="text" name="nis" id="nis" minlength="10" maxlength="10" required>
+                    </td>
+                </tr>
+                <tr class="alamat">
+                    <td>
+                        <label for="alamat">Alamat</label>
+                    </td>
+                    <td>
+                        <label for="alamat" class="label-titik-dua"> : </label>
+                    </td>
+                    <td>
+                        <input name="alamat" id="alamat" type="text" required>
+                    </td>
+                </tr>
+                <tr class="jenis-kelamin">
+                    <td>
+                        <label for="jenis_kelamin">Jenis Kelamin</label>
+                    </td>
+                    <td>
+                        <label for="jenis_kelamin" class="label-titik-dua"> : </label>
+                    </td>
+                    <td>
+                        <input name="jenisKelamin" id="jenis_kelamin" type="text" required>
+                    </td>
+                </tr>
+                <tr class="agama">
+                    <td>
+                        <label for="agama">Agama</label>
+                    </td>
+                    <td>
+                        <label for="agama" class="label-titik-dua"> : </label>
+                    </td>
+                    <td>
+                        <input name="agama" id="agama" type="text" required>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="3" class="tombol-simpan">
+                        <button type="submit" name="submit" class="tombol-simpan">Simpan</button>
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </div>
+
+</body>
+</html>
